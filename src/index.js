4bf8991ac87980d2364442d42b349f134e30e041
Vlad <vlados.pochta@gmail.com>
2020-05-08 00:41:32 +0300

    Upd index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ const app = express();
 
 app.get('/', async (req, res) => {
     res.send('Hello World');
-    console.log(req);
 });
-
-app.post('/', async (req, res) => {
-    const data = req.body;
-    console.log('data: ', data);
-    res.send('good!');
-});
-
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
