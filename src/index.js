50a3f667cabda204ba91b0f279c466c909427c56
Vlad <vlados.pochta@gmail.com>
2020-05-08 16:46:50 +0300

    Add JSON

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', async (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', async(req, res) => {
+    const data = req.body;
+    const result = data.A + data.B;
+    const result_json = {
+        'A': data.A,
+        'B': data.B,
+        'Result': result
+    };
+    console.log('Ok');
+    res.json(result_json);
 });
 
 app.listen(3000, () => {
