2eb2af5308349f2d18931372b3b6ca4b5b765040
Vlad <vlados.pochta@gmail.com>
2020-05-08 16:37:51 +0300

    Add A+B

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', async (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', async(req, res) => {
+    const data = req.body;
+    const result = data.A + data.B;
+    res.send(`Result: ${result}`)
 });
 
 app.listen(3000, () => {
